dffd1584b8e2ad9469a5301ac354143515488890
sunxikai <xikai.sun@pactera.com>
2018-07-20 15:21:22 +0800

    eureka-server

---
+++ eureka-server/src/main/java/sunxikai928/com/github/springcloud/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package sunxikai928.com.github.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// 启动一个服务注册中心，只需要一个注解@EnableEurekaServer
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
